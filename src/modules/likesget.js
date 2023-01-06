3222ec262b69d414fd58bb6604c20dc7ef7a4486
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-01-06 23:16:37 +0500

    Posting the data to likes

--- src/modules/likesget.js
+++ src/modules/likesget.js
@@ -1,14 +1,13 @@
 const getLikes = async () => {
-  const url =
-    "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/x1JnCaUrbuCma7kL4K33/likes";
+  const url = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/x1JnCaUrbuCma7kL4K33/likes?item_id=${}`;
   const response = await fetch(url);
   const data = await response.json();
-  const listItems = document.querySelector(".list-items");
+  const listItems = document.querySelector('.list-items');
   const getFunction = (data) => {
     data.results.forEach(() => {
-      const paragraph = document.createElement("p");
-      paragraph.classList.add("paras");
-      paragraph.innerText = "5 likes";
+      const paragraph = document.createElement('p');
+      paragraph.classList.add('paras');
+      paragraph.innerText = '5 likes';
       listItems.append(paragraph);
     });
   };
