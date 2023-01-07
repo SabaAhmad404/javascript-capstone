6970ca0a7b21692d34534bcf1d739bd893c6cf7c
george827 <kinyanjuigeorge827@gmail.com>
2023-01-07 09:04:56 +0300

    display the coments

--- src/modules/comments/getData.js
+++ src/modules/comments/getData.js
@@ -1,12 +1,32 @@
 const getData = (data) => {
   const id = 'item'.concat(data);
-  console.log(id, 'id');
 
   const userName = document.getElementById('User-name');
   const userComment = document.getElementById('User-comment');
   const submitComment = document.querySelector('.Csubmit');
- 
+
   const Mycomment = document.querySelector('.mComment');
+
+  const show = (data) => {
+    if (data.length > 0) {
+      Mycomment.innerHTML = '';
+      data.forEach((comment) => {
+        const li = document.createElement('li');
+        li.classList.add('newComentList');
+        Mycomment.append(li);
+        li.innerHTML += `${comment.creation_date} ${comment.username} : ${comment.comment}`;
+      });
+    } else {
+      Mycomment.innerHTML = '<span class="data">No data about this pokemon</span>';
+    }
+  };
+
+  const getpost = async () => {
+    const response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/QMi7IVnLIBx9LsDFnmic/comments?item_id='.concat(id));
+    const convert = await response.json();
+    show(convert);
+    return convert;
+  };
 
   submitComment.addEventListener('click', async () => {
     await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/QMi7IVnLIBx9LsDFnmic/comments',
@@ -24,28 +44,6 @@ const getData = (data) => {
       });
     getpost();
   });
-
-  const getpost = async () => {
-    const response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/QMi7IVnLIBx9LsDFnmic/comments?item_id='.concat(id));
-    const convert = await response.json();
-    show(convert);
-    return convert;
-  };
-
-  const show = (data) => {
-    if (data.length > 0) {
-      Mycomment.innerHTML = '';
-      data.forEach((comment) => {
-        const li = document.createElement('li');
-        li.classList.add("newComentList")
-        Mycomment.append(li);
-        li.innerHTML += `${comment.creation_date} ${comment.username} : ${comment.comment}`;
-      });
-    } else {
-      Mycomment.innerHTML = '<span class="data">No data about this pokemon</span>';
-    }
-  };
-
 };
 
 export default getData;
